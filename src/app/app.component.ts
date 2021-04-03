fd80ee2f428ac65b18da635d255ba9deaa56df29
unknown <manashbarman55@gmail.com>
2021-04-03 08:37:49 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {FormControl, FormGroup, Validators} from '@angular/forms'
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,22 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'myStudentRecord';
+
+  userProfileForm= new FormGroup({
+        firstname:new FormControl('',Validators.required),
+        lastname: new FormControl('',Validators.required),
+        age: new FormControl('',Validators.required),
+        email: new FormControl('',Validators.required),
+        password: new FormControl('',Validators.required)
+      });
+
+  onSubmit()
+      {
+        console.log(this.userProfileForm.value);
+
+        //when we just want to access 'age' field rather then accessing the whole form
+        //console.log(this.userProfileForm.controls['age'].value);
+        //or 
+        //console.log(this.userProfileForm.get('age').value);
+      }
 }
